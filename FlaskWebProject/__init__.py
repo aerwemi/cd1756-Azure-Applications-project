ec8c395e9201d803a512860eec94c8996571dba4
Naser Erwemi <aerwemi@me.com>
2026-04-08 19:11:04 -0500

    views

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -10,7 +10,14 @@ from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# TODO: Add any logging levels and handlers with app.logger
+# Add logging for authentication events
+app.logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+handler.setFormatter(formatter)
+if not app.logger.handlers:
+    app.logger.addHandler(handler)
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
